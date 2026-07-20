1ec6caf7461a8467f5b081f977b8d4414e0e1316
Shubham Mahalle <shubhammahallee@gmail.com>
2026-07-20 08:50:46 +0530

    Update streamlit_frontend.py

--- streamlit_frontend.py
+++ streamlit_frontend.py
@@ -23,7 +23,7 @@ if user_input:
     # first add the message to message_history
     st.session_state['message_history'].append({'role': 'user', 'content': user_input})
     with st.chat_message('user'):
-        st.text(user_input)
+        st.text(user_input) 
 
     response = chatbot.invoke({'messages': [HumanMessage(content=user_input)]}, config=CONFIG)
     
