929a2bae7a7f8706d92a8d8c6d9796475b8731c6
Shubham Mahalle <shubhammahallee@gmail.com>
2026-07-25 08:40:11 +0530

    Update streamlit_frontend_database.py

--- streamlit_frontend_database.py
+++ streamlit_frontend_database.py
@@ -13,7 +13,7 @@ def reset_chat():
     thread_id = generate_thread_id()
     st.session_state['thread_id'] = thread_id 
     add_thread(st.session_state['thread_id'])
-    st.session_state['message_history'] = []
+    st.session_state['message_history'] = [] 
 
 def add_thread(thread_id):
     if thread_id not in st.session_state['chat_threads']:
