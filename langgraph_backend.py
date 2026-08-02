5f527b2cdae3a14311bbcad030bfaa97c34ecdc6
Shubham Mahalle <shubhammahallee@gmail.com>
2026-08-02 16:38:23 +0530

    Update langgraph_backend.py

--- langgraph_backend.py
+++ langgraph_backend.py
@@ -29,4 +29,4 @@ graph.add_node("chat_node", chat_node)
 graph.add_edge(START, "chat_node")
 graph.add_edge("chat_node", END)
 
-chatbot = graph.compile(checkpointer=checkpointer)
+chatbot = graph.compile(checkpointer=checkpointer) 
