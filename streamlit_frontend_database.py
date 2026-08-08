5c3fb34bcaa3151c131cd36f85850c508b70dcff
Shubham Mahalle <shubhammahallee@gmail.com>
2026-08-08 23:00:40 +0530

    Update streamlit_frontend_database.py

--- streamlit_frontend_database.py
+++ streamlit_frontend_database.py
@@ -105,4 +105,4 @@ if user_input:
             )
         )
 
-    st.session_state['message_history'].append({'role': 'assistant', 'content': ai_message})
+    st.session_state['message_history'].append({'role': 'assistant', 'content': ai_message}) 
