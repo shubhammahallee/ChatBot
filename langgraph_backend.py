1d60031eff0a61b46cd1e63d91861dfa5b7f7cce
Shubham Mahalle <shubhammahallee@gmail.com>
2026-08-20 10:43:45 +0530

    Update langgraph_backend.py

--- langgraph_backend.py
+++ langgraph_backend.py
@@ -1,5 +1,5 @@
 from langgraph.graph import StateGraph, START, END  
-from typing import TypedDict, Annotated
+from typing import TypedDict, Annotated 
 from langchain_core.messages import BaseMessage
 from langchain_ollama import ChatOllama
 from langgraph.checkpoint.memory import InMemorySaver 
